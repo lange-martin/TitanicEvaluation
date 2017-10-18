8b52e290d564fc9aeca549520e0ae8377d67a529
Martin Lange <martinlange@Admins-iMac.fritz.box>
2017-10-18 13:06:05 +0200

    evaluation can now easily be chosen per file

--- complete/src/main/java/hello/EvaluationController.java
+++ complete/src/main/java/hello/EvaluationController.java
@@ -1,10 +1,7 @@
 package hello;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
 import java.io.IOException;
@@ -12,14 +9,9 @@ import java.io.IOException;
 @Controller
 public class EvaluationController {
 
-    @GetMapping("/eval")
-    public String evaluate(Model model) {
-        model.addAttribute("evaluator", new Evaluator());
-        return "evaluation";
-    }
-
-    @PostMapping("/eval")
-    public String showResult(@ModelAttribute Evaluator evaluator) {
+    @GetMapping("/files/{filename:.+}/eval")
+    public String testFile(@ModelAttribute Evaluator evaluator, @PathVariable String filename) {
+        evaluator.setPath(filename);
         try {
             evaluator.run();
         } catch (IOException e) {
